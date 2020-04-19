b997e54b5196ba2d3300bd65630ab3f61dbbc2f7
Renato Fabbri <renato.fabbri@adroll.com>
2020-04-19 11:47:30 -0300

    minor

--- visual_analytics/scripts/modules/artist.js
+++ visual_analytics/scripts/modules/artist.js
@@ -1,7 +1,7 @@
 var draw = require('./drawing/main.js')
 window.mdraw = draw.use
 
-let testing = 0
+let testing = 1
 if (testing) {
     console.log(draw.use)
     draw.share.test.tests.backImage(draw.use.ambience, draw.share.base.PIXI, draw.share.base.app)
@@ -10,7 +10,8 @@ if (testing) {
     draw.share.test.tests.dancers(draw.use.ambience, draw.share.base.app)
     draw.share.test.tests.oneNode(draw.use)
     draw.share.test.tests.basicElements(draw.use)
+} else {
+  draw.share.test.tests.basicElements(draw.use)
 }
-draw.share.test.tests.basicElements(draw.use)
 
-exports = { use: draw.use, share: draw.share }
+module.exports = { use: draw.use, share: draw.share }

bd9c3c774e35c4123104f6e0e2cab03f1d466757
Renato Fabbri <renato.fabbri@gmail.com>
2020-06-10 10:15:03 -0300

    finished debugging, cleaning and simplifying scrapping

--- visual_analytics/extension/scripts/background.js
+++ visual_analytics/extension/scripts/background.js
@@ -1,32 +1,54 @@
-//To issue message to begin scrolling
+let scrappedData
+let counter
+
+const getMutualFriendsURL = (profile) => {
+  if (profile.numericId === undefined) {
+    return `${profile.url}/friends_mutual`
+  } else {
+    return `${profile.url}&sk=friends_mutual`
+  }
+}
+
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.sendMessage(tab.id, { "message": "clicked_browser_action" })
+  scrappedData = { profiles: [], pageUserData: [], seed: undefined }
+  counter = 0
+  chrome.tabs.sendMessage(tab.id, { message: 'clicked_browser_action' })
 })
 
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === 'open_new_tab') {
-            console.log('received open new tab')
-            chrome.tabs.create({ url: request.url }, function (tab) {
-                console.log('tab open mannn', request.all_profiles, 'PROFs')
-                chrome.tabs.executeScript(tab.id, { file: 'scripts/fb_scrape.js' }, function () {
-                    chrome.tabs.sendMessage(tab.id, { 'message': 'opened_new_tab', all_profiles: request.profiles })
-                })
-                // chrome.tabs.sendMessage(tab.id, { message: 'opened_new_tab', all_profiles })
-
-            })
+  function (request, sender, sendResponse) {
+    if (request.message === 'open_new_tab') {
+      if (request.pageUserData !== undefined) {
+        if (scrappedData.seed === undefined) { // seed arrived
+          scrappedData.seed = {
+            userInfo: request.pageUserData,
+            profiles: request.profiles
+          }
+        } else { // iterating
+          scrappedData.pageUserData.push(request.pageUserData)
+          scrappedData.profiles.push(...request.profiles)
         }
-    }
-)
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === 'close_current_tab') {
-            chrome.tabs.getSelected(null, function (tab) {
-                all_profiles.push(request.profiles)
-                console.log(all_profiles, '<=== all_profiles')
-                // chrome.tabs.remove(tab.id);
+        if (counter < scrappedData.seed.profiles.length) {
+          const profile = scrappedData.seed.profiles[counter++]
+          console.log('profile', profile, getMutualFriendsURL(profile))
+          chrome.tabs.getSelected(null, function (tab) {
+            // chrome.tabs.remove(tab.id);
+            chrome.tabs.create({ url: getMutualFriendsURL(profile) }, function (tab) {
+              chrome.tabs.executeScript(tab.id, { file: 'scripts/fb_scrape.js' }, function () {
+                chrome.tabs.sendMessage(tab.id, { message: 'opened_new_tab' })
+              })
             })
+          })
+        } else {
+          console.log('finished scrapping iteration')
         }
+      } else { // just clicked, first page to scrape
+        chrome.tabs.create({ url: request.url }, function (tab) {
+          chrome.tabs.executeScript(tab.id, { file: 'scripts/fb_scrape.js' }, function () {
+            chrome.tabs.sendMessage(tab.id, { message: 'opened_new_tab' })
+          })
+        })
+      }
     }
+  }
 )
--- visual_analytics/extension/scripts/fb_scrape.js
+++ visual_analytics/extension/scripts/fb_scrape.js
@@ -1,46 +1,40 @@
-console.log('inside fb to scrape man')
-// chrome.runtime.sendMessage('YOOOO', function (response) {
-//   console.log(response.received_by.concat(' heard me.'))
-// })
-
-
 var hitsCounterThreshold = 3; //Recommended:10
 var initDelayInMilliseconds = 2000; //Recommended:5000
 var scrollDelayInMilliSeconds = 500; //Recommended:1000
 var scrollMagnitude = 1000; //Recommended:1000
 
 function scrollTillEnd(call = () => console.log('scrolling complete')) {
-    var x = 1, y = -1;
-    var hitsCounter = 0;
-    time = setInterval(function () {
-        x = document.documentElement.scrollTop;
-        document.documentElement.scrollTop += scrollMagnitude;
-        y = document.documentElement.scrollTop;
-        if (x == y) {
-            hitsCounter += 1;
-            if (hitsCounter > hitsCounterThreshold) {
-                clearInterval(time);
-                console.log('finished scrolling')
-                call()
-            }
-        }
-        else {
-            hitsCounter = 0;
-        }
-    }, scrollDelayInMilliSeconds);
+  var x = 1, y = -1;
+  var hitsCounter = 0;
+  time = setInterval(function () {
+    x = document.documentElement.scrollTop;
+    document.documentElement.scrollTop += scrollMagnitude;
+    y = document.documentElement.scrollTop;
+    if (x == y) {
+      hitsCounter += 1;
+      if (hitsCounter > hitsCounterThreshold) {
+        clearInterval(time);
+        console.log('finished scrolling')
+        call()
+      }
+    }
+    else {
+      hitsCounter = 0;
+    }
+  }, scrollDelayInMilliSeconds)
 }
 
 function getElementsByXPath(xpath, parent) {
-    var results = [];
-    var query = document.evaluate(xpath, parent || document,
-        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    for (var i = 0, length = query.snapshotLength; i < length; ++i) {
-        results.push(query.snapshotItem(i));
-    }
-    return results;
+  var results = [];
+  var query = document.evaluate(xpath, parent || document,
+    null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+  for (var i = 0, length = query.snapshotLength; i < length; ++i) {
+    results.push(query.snapshotItem(i))
+  }
+  return results
 }
 
-function startupOk() {
+function getUserPageData() {
   var membername = getElementsByXPath('//*/h1').map(i => i.innerText)[0]
   var parts = membername.match(/[^\r\n]+/g)
   var name = parts[0]
@@ -57,53 +51,6 @@ function startupOk() {
     name,
     codename
   }
-}
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "clicked_browser_action") {
-            // var firstHref = $("a[href^='http']").eq(0).attr("href");
-            let url = window.location.href
-            chrome.runtime.sendMessage({ message: 'open_new_tab', url, all_profiles: [] });
-        }
-    }
-);
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "opened_new_tab") {
-          console.log(request.all_profiles, 'ALL PROFs')
-          window.all_profiles = request.all_profiles
-          scrape(request.all_profiles)
-        }
-    }
-);
-
-function getFriendsProfiles (profiles) {
-  setTimeout(() => {
-  // window.open(url,'_self')
-  // chrome.runtime.sendMessage({ "message": "open_new_tab", "url": url })
-    scrollTillEnd(() => {
-      // parse HTML to get profiles:
-      var startData = startupOk()
-      console.log(startData, 'HAAAA')
-      var profiles_ = htmlToFriendsProfiles()
-      var url = `${profiles_[0].url}/friends_mutual`
-      profiles.push(...profiles_)
-      console.log('profiles', profiles, url)
-      window.profs = profiles
-      // window.open(url,'_self')
-      // chrome.runtime.sendMessage({ "message": "open_new_tab", "url": url })
-      closeCurrentTab(profiles)
-      chrome.runtime.sendMessage({ "message": "open_new_tab", "url": url, profiles });
-      console.log(startData, 'HOOO')
-    })
-  }, initDelayInMilliseconds);
-}
-
-function closeCurrentTab(profiles) {
-    console.log('Tab termination message issued.')
-    chrome.runtime.sendMessage({ message: 'close_current_tab', profiles });
 }
 
 function htmlToFriendsProfiles () {
@@ -165,39 +112,29 @@ function htmlToFriendsProfiles () {
   return iids
 }
 
-function scrape (profiles) {
-  // starts with the profile page of the seed, preferentially / typically the user
-  // get name and id
-  // enter and scrape friends page
-  // for each friend:
-  //    enters its friends or friends_mutual page
-  //    get name with potential codename
-  //    assert gotten name matches name if id already scrapped
-  //    scrape friends
-  //    if not friends_mutual:
-  //        may wish to keep track of ppl with are not friends with the seed
-  //        to iterate and get friends
-  var seedProfiles = getFriendsProfiles(profiles)
-  if (true) {
-    return
-  }
-  return {
-    seed: [sid, nid],
-    seedFriendIds: {
-      string: stringSeedFriendIds,
-      numeric: numericSeedFriendIds,
-      ids: seedFriendIds, // string if available, else numeric, else undefined
-      mutual: false, // if true, start from the mutual friends of the seed, for seed not logged in fb
-    },
-    membersIds: {
-      numericIds, // of any member scrapped that is not the seed
-      stringIds, // of any member scrapped that is not the seed
-      ids,
-      mutual: true, // if true, start from the mutual friends of the seed, for seed not logged in fb
-    },
-    friendships, // sequence of [id1, id2]
-    profiles, // list of dicts with {id, numericId, stringId, name, profilePage, mutualFriends
-  }
+function scrape () {
+  setTimeout(() => {
+    var pageUserData = getUserPageData()
+    scrollTillEnd(() => {
+      var profiles = htmlToFriendsProfiles()
+      chrome.runtime.sendMessage({ message: 'open_new_tab', pageUserData, profiles })
+    })
+  }, initDelayInMilliseconds)
 }
 
-// scrape()
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.message === 'clicked_browser_action') {
+      let url = window.location.href // make url for mutual friends, both numeric and string id
+      chrome.runtime.sendMessage({ message: 'open_new_tab', url })
+    }
+  }
+)
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.message === 'opened_new_tab') {
+      scrape()
+    }
+  }
+)

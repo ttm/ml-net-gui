09163a6d6dd29a17eac3cb095a917dcd22f20cb9
Renato Fabbri <renato.fabbri@gmail.com>
2020-08-20 05:18:21 -0300

    starting third extension

---
+++ visual_analytics/OAextension/pop.js
@@ -0,0 +1,40 @@
+/* global chrome */
+const checkPageButton = document.getElementById('advance')
+checkPageButton.style.backgroundColor = '#ffaaaa'
+checkPageButton.onclick = function (element) {
+  chrome.runtime.sendMessage({ message: 'popup_advance_msg' })
+}
+
+const gradusButton = document.getElementById('gradus')
+gradusButton.style.backgroundColor = '#ffffaa'
+gradusButton.onclick = function (element) {
+  chrome.runtime.sendMessage({ message: 'popup_gradus_msg' })
+}
+
+const lycoreiaButton = document.getElementById('lycoreia')
+lycoreiaButton.style.backgroundColor = '#ffffaa'
+lycoreiaButton.onclick = function (element) {
+  chrome.runtime.sendMessage({ message: 'popup_lycoreia_msg' })
+}
+
+const tithoreaButton = document.getElementById('tithorea')
+tithoreaButton.style.backgroundColor = '#ffffaa'
+tithoreaButton.onclick = function (element) {
+  chrome.runtime.sendMessage({ message: 'popup_tithorea_msg' })
+}
+
+const name = document.getElementById('sage-name')
+const id = document.getElementById('sage-id')
+const nfriends = document.getElementById('nfriends')
+const friendsVisited = document.getElementById('friends-visited')
+const friendships = document.getElementById('friendships')
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get(['scrapeStatus'], r => {
+    const i = r.scrapeStatus
+    name.innerHTML = i.name
+    id.innerHTML = i.sid || i.nid
+    nfriends.innerHTML = i.nfriends
+    friendsVisited.innerHTML = i.friendsVisited
+    friendships.innerHTML = i.friendships
+  })
+})
